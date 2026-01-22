12aa2a732493e6f3bf1540c1f6cd1e61e46181d0
Nick Knipe <nick@hellodrifter.com>
2026-01-21 19:22:22 -0800

    Initial commit

---
+++ Formula/wralph.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Wralph < Formula
+  desc 'Workflow Ralph - Human-In-The-Loop AI Factory'
+  homepage 'https://github.com/niborg/wralph'
+  url 'https://github.com/niborg/wralph/archive/v0.1.0.tar.gz'
+  sha256 '8e5c52da5fb8d6c52e94c8ad47bbc78943b45829c840232468d5a2a9af0d3462'
+  license 'MIT'
+
+  depends_on 'ruby'
+  depends_on 'gh'
+  depends_on 'jq'
+  depends_on 'max-sixty/worktrunk/wt'
+
+  def install
+    # 1. Install gem dependencies into a private folder (libexec)
+    ENV["GEM_HOME"] = libexec
+
+    # 2. Build your gem from the gemspec
+    system "gem", "build", "#{name}.gemspec"
+
+    # 3. Install it into the private folder
+    system "gem", "install", "#{name}-*.gem"
+
+    # 4. Install the executable into the public bin folder,
+    #    but wrap it so it knows where its private gems (libexec) are.
+    bin.install libexec/"bin/wralph"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    system "#{bin}/wralph", '--version'
+  end
+end
